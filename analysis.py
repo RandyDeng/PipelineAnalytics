3e4e53abdaf47df254656320d8fda8c419ffd53b
Saul <saul.crumpton@gatech.edu>
2018-04-28 21:09:17 -0400

    working analysis pushing to BigQuery

--- analysis.py
+++ analysis.py
@@ -18,26 +18,32 @@ def batch(messages):
     for line in messages:
         for sw in special_words:
             key_words[sw] = 0
-        wordCount = 0
-        charCount = 0
+        WordCount = 0
+        CharCount = 0
         post = line.split(",")
         words = post[2].split(" ")
         for posttext in post[2]:
-            charCount = charCount+1
+            CharCount = CharCount+1
         for word in words:
             if(key_words.get(word, None) is not None):
                 key_words[word] += 1
-        wordCount = wordCount+len(words)
+        WordCount = WordCount+len(words)
         if(data.get(post[0])):
             for word in key_words:
                 data[post[0]][word] += key_words[word]
-            data[post[0]]["wordCount"] += wordCount
-            data[post[0]]["wordCount"] += charCount
-            data[post[0]]["postCount"] += 1
+            data[post[0]]["WordCount"] += WordCount
+            data[post[0]]["CharCount"] += CharCount
+            data[post[0]]["TweetCount"] += 1
         else:
             data[post[0]] = {}
             for word in key_words:
                 data[post[0]][word] = key_words[word]
-            data[post[0]]["wordCount"] = wordCount
-            data[post[0]]["wordCount"] += charCount
-            data[post[0]]["postCount"] = 1
+            data[post[0]]["WordCount"] = WordCount
+            data[post[0]]["CharCount"] = CharCount
+            data[post[0]]["TweetCount"] = 1
+            data[post[0]]["StandardWords"] = 0
+    for user in data:
+#        print(user)
+        query="INSERT INTO pipelineanalytics.word_count (Username,WordCount,CharCount, TweetCount, StandardWords, Randy_Deng, Saul_Crumpton, Eric_Daigrepont, Doug_Blough, Georgia_Tech, Google_Cloud, Baymax, Mario_Brothers, Marvel_vs_DC, Dungeons_and_Dragons) Values ('%s',%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d);" %(user,data[user]["WordCount"],data[user]["CharCount"],data[user]["TweetCount"],data[user]["StandardWords"],data[user]["Randy_Deng"],data[user]["Saul_Crumpton"],data[user]["Eric_Daigrepont"],data[user]["Doug_Blough"],data[user]["Georgia_Tech"],data[user]["Google_Cloud"],data[user]["Baymax"],data[user]["Mario_Brothers"],data[user]["Marvel_vs_DC"],data[user]["Dungeons_and_Dragons"])
+        print(query)
+        query_standard_sql(query)        
