a52a2f67292f4c988330408c9d06592f9829e552
bcrumpton3 <bcrumpton3@gatech.edu>
2018-04-30 15:28:23 -0700

    correct skeleton code for 'student'

--- SkeletonCode/pipelineanalytics.py
+++ SkeletonCode/pipelineanalytics.py
@@ -14,23 +14,14 @@ message_q = queue.Queue()
 
 
 def receive_message(message):
-    print('Received message: {}'.format(message))
-    message.ack()
-    message_q.put(message)
+
 
 
 if __name__ == '__main__':
     # Begin callback loop to receive messages
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(
-        PROJECT, SUBSCRIPTION)
-    subscriber.subscribe(subscription_path, callback=receive_message)
 
     # The subscriber is non-blocking, so we must keep the main thread from
     # exiting to allow it to process messages in the background
     while True:
         # Analyze the data in the queue every ANALYSIS_INTERVAL
-        time.sleep(ANALYSIS_INTERVAL)
-        size = message_q.qsize()
-        messages = [message_q.get().data for i in range(size)]
-        analysis.batch(messages)
+
--- SkeletonCode/publisher.py
+++ SkeletonCode/publisher.py
@@ -8,20 +8,13 @@ SLEEP_TIME = 1
 
 
 def publish_message(project=PROJECT, topic_name=TOPIC_NAME):
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project, topic_name)
 
-    data = generator.generate_post()
-    data = data.encode('utf-8')
-    publisher.publish(topic_path, data=data)
 
-    print('Published message: {}'.format(data))
 
 
 def simulate_datastream():
     while True:
-        publish_message(PROJECT, TOPIC_NAME)
-        time.sleep(SLEEP_TIME)
+
 
 
 if __name__ == '__main__':
